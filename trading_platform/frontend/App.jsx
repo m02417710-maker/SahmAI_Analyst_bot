e03b12bf12bf113469301c8497c33b1ef11dd6f1
m02417710-maker <m02417710@gmail.com>
2026-05-05 21:50:53 +0300

    Update App.jsx
    Signed-off-by: m02417710-maker <m02417710@gmail.com>

--- trading_platform/frontend/App.jsx
+++ trading_platform/frontend/App.jsx
@@ -3,7 +3,6 @@
 المسار: /trading_platform/frontend/App.jsx
 الوظيفة: واجهة المستخدم الرئيسية - لوحة التحكم
 */
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {
